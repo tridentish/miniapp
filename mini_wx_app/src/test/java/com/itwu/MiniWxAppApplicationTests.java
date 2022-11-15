b824a3c3c0149ade323a61f5c942e88a38ad7bd4
2368416713 <2368416713@qq.com>
2022-11-15 22:16:34 +0800

    把文件加入控制

---
+++ mini_wx_app/src/test/java/com/itwu/MiniWxAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itwu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniWxAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

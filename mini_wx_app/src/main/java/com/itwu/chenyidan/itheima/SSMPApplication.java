b2ee3038b4e7df6b092d5427c77a4ae305264087
yidan chen <18965292861@aliyun.com>
2022-11-23 12:33:47 +0800

    d

--- mini_wx_app/src/main/java/com/itwu/chenyidan/itheima/SSMPApplication.java
+++ mini_wx_app/src/main/java/com/itwu/chenyidan/itheima/SSMPApplication.java
@@ -1,13 +1,13 @@
-//package com.itwu.chenyidan.itheima;
-//
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
-//
-//@SpringBootApplication
-//public class SSMPApplication {
-//
-//    public static void main(String[] args) {
-//        SpringApplication.run(SSMPApplication.class, args);
-//    }
-//
-//}
+package com.itwu.chenyidan.itheima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SSMPApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SSMPApplication.class, args);
+    }
+
+}

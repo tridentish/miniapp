f172768204d2e1c364bf367e7ae5e57f2a5c2036
yidan chen <18965292861@aliyun.com>
2022-11-25 16:59:24 +0800

    d

--- mini_wx_app/src/main/java/com/itwu/chenyidan/itheima/service/IUserService.java
+++ mini_wx_app/src/main/java/com/itwu/chenyidan/itheima/service/IUserService.java
@@ -4,22 +4,4 @@
 //import com.baomidou.mybatisplus.extension.service.IService;
 //import com.itheima.domain.User;
 //
-//import java.util.List;
-//
-//public interface IUserService extends IService<User> {
-//    boolean saveUser(User user);
-//
-//    boolean modify(User user);
-//
-//    boolean update(User user);
-//
-//    boolean delete(Integer id);
-//
-//    User getById(Integer id);
-//
-//    List<User> getAll();
-//
-//    IPage<User> getPage(int currentPage, int pageSize);
-//
-//    IPage<User> getPage(int currentPage, int pageSize, User user);
-//}
+
